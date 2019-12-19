047eb62dfaf08d112d7e1187a8ff5358be5fc3d0
vasyl <bz.vasok@gmail.com>
2019-12-19 16:49:42 +0200

    Updates

---
+++ precache-manifest.805d2beaaf8d1dab13d5a512820fd0e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c940b624e34dc16a913950015404801",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "e310bc6a5a97d8515c87",
+    "url": "/test-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cf1f34b96479781184a8",
+    "url": "/test-app/static/js/2.994851ff.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/test-app/static/js/2.994851ff.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e310bc6a5a97d8515c87",
+    "url": "/test-app/static/js/main.5cf4fbc5.chunk.js"
+  },
+  {
+    "revision": "fd13b0ec5f949406ab0e",
+    "url": "/test-app/static/js/runtime-main.8424f9d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
