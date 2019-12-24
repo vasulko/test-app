cbd98e7babc9e06f94a1196ea2442c8004925fa6
Unknown <bz.vasok@gmail.com>
2019-12-24 23:33:59 +0200

    delete console.log in navigation-item

--- src/components/navigation-item/navigation-item.js
+++ src/components/navigation-item/navigation-item.js
@@ -5,7 +5,6 @@ import './navigation-item.css';
 const NavigationItem = ({ label, caption = null }) => {
     const element = caption ? <span className='navigation__caption'>{label}</span>
                             : <a href='#' className='navigation__link'>{ label }</a>;
-    console.log(element);
     return(
         <>
             {element}
